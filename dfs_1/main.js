8ae6057c0790e827c02c7fbc54303c247daf021d
Aaron Dabney <aarongdabney@gmail.com>
2024-10-18 12:32:40 -0700

    misc

--- dfs_1/main.js
+++ dfs_1/main.js
@@ -2,12 +2,15 @@ const { Graph } = require('./graph')
 const { people } = require('./people')
 
 
-let graph = new Graph();
+const graph = new Graph();
 
+
+// Add vertices to graph
 for (let person of people) {
     graph.addVertex(person);
 }
 
+// Add edges to graph
 for (let person of people) {
     for (let friendName of person.friends) {
         let friend = people.find(el => el.name === friendName);
@@ -16,7 +19,7 @@ for (let person of people) {
 }
 
 /**
- * Return all matches from graph that pass test function
+ * Return an array of matching nodes from graph that pass test function
  */
 function bfs(graph, startingNode, testFunc) {
     let discovered = new Set([startingNode])
@@ -46,8 +49,35 @@ function bfs(graph, startingNode, testFunc) {
     return matches;
 }
 
+
+
 const isFriendsWithSally = personNode => personNode.friends.includes('Sally');
 
-let sally = people.find(el => el.name = 'Sally');
+const sally = people.find(el => el.name = 'Sally');
 
+
+console.log(graph);
+console.log('\n')
+console.log('Matches: ')
 console.log(bfs(graph, sally, isFriendsWithSally));
+
+
+// Output -> 
+
+// Graph {
+//     adjList: Map(6) {
+//       { name: 'Sally', age: 20, friends: [Array] } => [ [Object], [Object], [Object] ],
+//       { name: 'Bob', age: 202, friends: [Array] } => [ [Object], [Object] ],
+//       { name: 'Mike', age: 2, friends: [Array] } => [ [Object], [Object] ],
+//       { name: 'Dave', age: 19, friends: [Array] } => [ [Object], [Object] ],
+//       { name: 'Jim', age: 19, friends: [Array] } => [ [Object], [Object] ],
+//       { name: 'Havier', age: 13, friends: [Array] } => [ [Object] ]
+//     }
+//   }
+  
+  
+//   Matches: 
+//   [
+//     { name: 'Dave', age: 19, friends: [ 'Sally' ] },
+//     { name: 'Jim', age: 19, friends: [ 'Sally' ] }
+//   ]
