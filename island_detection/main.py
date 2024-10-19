618035e34baedebaa69aa0468f886b85bff6c53c
Aaron Dabney <aarongdabney@gmail.com>
2024-10-19 10:54:47 -0700

    removed try except

--- island_detection/main.py
+++ island_detection/main.py
@@ -35,41 +35,37 @@ def islandDetector(gridInput):
     width = len(grid[0])
     height = len(grid)
 
-    offsets = [[0,  1],
-               [1,  1],
-               [1,  0],
-               [1, -1],
-               [0, -1],
+    offsets = [[ 0, 1],
+               [ 1, 1],
+               [ 1, 0],
+               [ 1,-1],
+               [ 0,-1],
                [-1,-1],
                [-1, 0],
                [-1, 1]]
-    
 
     def inBounds(x, y):
         if (x < 0 or y < 0):
             return False
-        if ((x >= width or y >= height)):
+        if (x >= width or y >= height):
             return False
         return True
 
     def getGrid(x, y):
-        if (not inBounds(x, y)):
-            raise "Out of bounds"
         return grid[y][x]
 
     def setGrid(x, y, val):
-        if (not inBounds(x, y)):
-            raise "Out of bounds"
         grid[y][x] = val
 
     def searchAndZero(x, y):
-        try:
-            if (getGrid(x, y)):
-                setGrid(x, y, 0)
-                for offset in offsets:
-                    searchAndZero(x + offset[0], y + offset[1])
-        except:
+        if (not inBounds(x, y)):
             return
+        if (getGrid(x, y)):
+            setGrid(x, y, 0)
+            for offset in offsets:
+                xOffset = offset[0]
+                yOffset = offset[1]
+                searchAndZero(x + xOffset, y + yOffset)
 
 
     islandCount = 0
@@ -85,7 +81,6 @@ def islandDetector(gridInput):
 
 
 for grid in gridData:
-    #print(grid)
     print(islandDetector(grid))
 
 # Output -> 1, 2, 4, 5, 1
